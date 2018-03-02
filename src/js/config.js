0beeb8291e265e47183b825c13ddb1fe097cda22
Ablay Keldibek <atomio.ak@gmail.com>
2018-03-02 14:26:50 +0100

    templates fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  imgProcessorBackend: 'http://127.0.0.1:3000/api/newsletter/image_processor.json',
-  emailProcessorBackend: 'http://127.0.0.1:3000/api/newsletter/email_processor.json',
-  emailProcessorLoader: 'http://127.0.0.1:3000/api/newsletter/email_processor/',
+  imgProcessorBackend: 'https://bart.nebenan.de/api/newsletter/image_processor.json',
+  emailProcessorBackend: 'https://bart.nebenan.de/api/newsletter/email_processor.json',
+  emailProcessorLoader: 'https://bart.nebenan.de/api/newsletter/email_processor/',
   titleToken: "MOSAICO - nebenan - Responsive Email Designer",
-  fileuploadConfigurl: 'http://127.0.0.1:3000/api/newsletter/file_upload.json',
-  getTemplateUrlPrefix: 'http://127.0.0.1:3000/api/newsletter/email_processor/',
-  boLink: 'http://127.0.0.1:3000/nebenadmin/nebenadmin/newsletter_template/',
+  fileuploadConfigurl: 'https://bart.nebenan.de/api/newsletter/file_upload.json',
+  getTemplateUrlPrefix: 'https://bart.nebenan.de/api/newsletter/email_processor/',
+  boLink: 'https://bart.nebenan.de/nebenadmin/nebenadmin/newsletter_template/'
 };
