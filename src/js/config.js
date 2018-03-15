da335cc91c377b6f33e63445b20b34b3bdd3c6c6
Ablay Keldibek <atomio.ak@gmail.com>
2018-03-15 22:16:29 +0100

    fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   api_root: 'https://bart.nebenan.de',
-  static_root: 'https://bart.nebenan.de',
+  static_root: 'https://static.nebenan.de/assets',
   titleToken: "MOSAICO - nebenan - Responsive Email Designer",
 };
